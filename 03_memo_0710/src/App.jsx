5f2ffc1fd2a67f9e2b2bb44b50c74a3114a2eb16
YoshimotoKatsuhiro <107968925+YoshimotoKatsuhiro@users.noreply.github.com>
2022-07-16 18:24:55 +0900

    課題再提出

--- 03_memo_0710/src/App.jsx
+++ 03_memo_0710/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css'
+import './reset.css'
+// npm install @mui/material @emotion/react @emotion/styled 実行
 import Button from '@mui/material/Button';
 
 function App() {
@@ -13,15 +15,21 @@ function App() {
   };
 
  
-  
   // 登録されるデータを保持するuseState
   const [data, setData] = useState(getData);
 
   // タイトル入力欄のテキスト情報を保持するuseState
-  const [title, setTitle] =useState("");
+  const [name, setName] =useState("");
 
-  // 追加 input(ここの名前は自分で決めてOK)  
+  const [kana, setKana] =useState("");
+
   const [email, setEmail] = useState("");
+
+  const [cue, setCue] = useState("");
+
+  const [reason, setReason] =useState("");
+
+  const [detail, setDetail] =useState("");
 
   // 送信を押したら登録
   const handleAddSubmit = (e) => {
@@ -30,12 +38,20 @@ function App() {
 
   //　データを登録するための「塊＝オブジェクト」を作る
   let pushData = {
-    title,
-    email, // 追加　inputのuseState
+    name,
+    kana,
+    email, 
+    cue,
+    reason,
+    detail,// 追加　inputのuseState
   };
   setData([...data, pushData]);
-  setTitle("");
+  setName("");
+  setKana("");
   setEmail("");
+  setCue("");
+  setReason("");
+  setDetail("");
   };
 
 
@@ -44,9 +60,14 @@ function App() {
     localStorage.setItem("test", JSON.stringify(data));
   }, [data]);
 
-  const handleDelete =() => {
-    localStorage.removeItem("test");
-  };
+
+    const allDelete =() => {
+      localStorage.clear();  
+   };
+
+  // const handleDelete =() => {
+  //   localStorage.removeItem(("test") => item.index !=index);
+  // };
 
   // const handleDelete = (id) => {
   //   /* 現在のタスクを削除する。 */
@@ -56,46 +77,154 @@ function App() {
 
   return (
     <div className="App">
-      {/* ここから書いていきます */}
-      <h1>localStrage</h1>
-      {/*  */}
-      <form onSubmit={handleAddSubmit}>
-        {/* title */}
-        <label>タイトル入力</label>
-        <input
-         type="text"
-         required
-         onChange={(e) => setTitle(e.target.value)}
-         value={title}
-         />
-        <hr />
-        {/* email */}
-        <label>追加したinput</label>
-        <input
-         type="text"
-         required
-         onChange={(e) => setEmail(e.target.value)}
-         value={email}
-         />
 
-        <hr />
-        {/* 送信ボタン */}
-        {/* <button type="submit">送信</button> */}
-        <Button type="submit" variant="contained">送信</Button>
-        
-        
-      </form>
+    {/* <section id="contact"> */}
+          <div className="course-wrapper">
+            <h2 className="text-center contact__title">Contact</h2>
+            <h3 className="text-center contact__text">説明会お申し込み・お問い合わせ</h3>
+            <p className="text-center contact__smalltext">ぜひ1度、足を運んでみませんか。説明会は随時開催中。</p>
+            <p className="text-center contact__smalltext">その他、お問い合わせもお気軽にどうぞ。お待ちしております。</p>
+            <p className="text-center contact__smalltext">※チーズアカデミーは実際には存在しません。</p>
+            <p className="text-center contact__smalltext bottom">間違っても問い合わせしないようお願いいたします。</p>
+            {/* <form action="#" method="POST"> */}
+            <form onSubmit={handleAddSubmit}>
+              <dl className="contact__list">
+                  <dt className="contact__dt">名前</dt>
+                  <dd className="contact__dd">
+                    <input 
+                    type="text" 
+                    // name="name" 
+                    // id="name" 
+                    className="form-parts" 
+                    required
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                    />
+                  </dd>
+                  <dt className="contact__dt">カナ</dt>
+                  <dd className="contact__dd">
+                    <input 
+                    type="text" 
+                    // name="name" 
+                    // id="name" 
+                    className="form-parts" 
+                    required
+                    onChange={(e) => setKana(e.target.value)}
+                    value={kana}
+                    />
+                    </dd>
+                  <dt className="contact__dt">メールアドレス</dt>
+                  <dd className="contact__dd">
+                    <input 
+                    type="email" 
+                    // name="email" 
+                    // id="email" 
+                    className="form-parts" 
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    /></dd>
+                  <dt className="contact__dt trigger">チーズアカデミーを知ったきっかけ</dt>
+                  <dd className="contact__dd">
+                    <select 
+                    name="selectbox" 
+                    className="form-parts"
+                    required
+                    onChange={(e) => setCue(e.target.value)} >
+                        <option value="#"></option>
+                        <option value="google検索">google検索</option>
+                        <option value="SNS">SNS</option>
+                        <option value="紹介">紹介</option>
+                        <option value="たまたま通りかかった">たまたま通りかかった</option>
+                        <option value="その他">その他</option>
+                    </select>
+                  </dd>
+                  <dt className="contact__dt check-radio">志望動機</dt>
+                  <dd className="contact__dd contact__radio">
+                      <label for="startup" className="form-label">
+                        <input 
+                        type="checkbox" 
+                        name="kind" 
+                        id="startup" 
+                        onChange={(e) => setReason(e.target.value)}
+                        value="起業をしたい" />
+                        起業をしたい
+                      </label>
+                      <label for="jobchange" className="form-label">
+                        <input 
+                        type="checkbox" 
+                        name="kind" 
+                        id="jobchange" 
+                        onChange={(e) => setReason(e.target.value)}
+                        value="チーズ系起業に就職・転職したい" />
+                        チーズ系企業に就職・転職したい
+                      </label>
+                      <label for="skillup" className="form-label">
+                        <input 
+                        type="checkbox" 
+                        name="kind" 
+                        id="skillup"
+                        onChange={(e) => setReason(e.target.value)} 
+                        value="チーズと関わる仕事をしており、仕事に生かしたい" />
+                        チーズと関わる仕事をしており、仕事に生かしたい
+                      </label>
+                      <label for="education" className="form-label">
+                        <input 
+                        type="checkbox" 
+                        name="kind" 
+                        id="education"
+                        onChange={(e) => setReason(e.target.value)} 
+                        value="チーズの教養を身に着けたい" />
+                        チーズの教養を身に着けたい
+                      </label>
+                  </dd>
+                  <dt className="contact__dt bottom">詳細</dt>
+                  <dd className="contact__dd bottom">
+                      <textarea 
+                      // name="detail" 
+                      // id="detail" 
+                      cols="30" 
+                      rows="10" 
+                      className="form-parts"
+                      required
+                      onChange={(e) => setDetail(e.target.value)}
+                      value={detail}
+                      >
+                      </textarea>
+                  </dd>
+              </dl>
+                <Button type="submit" variant="contained">送信</Button>
+                {/* <input type="submit" value="送信" className="btn btn-submit" /> */}
+            </form>
+          </div> 
+       {/* </section> */}
+
+
 
       {/* dataというuseStateの塊を、es6のmapというおまじないを使って表示しています */}
       {/* itemは任意で決める、a,でもよい */}
       {data.map((item, index) => (
         <div key={index}>
-          <p>タイトル：{item.title}</p>
-          <p>追加したもの：{item.email}</p>
-          <Button onClick={()=>handleDelete()}>削除</Button>
+          {/* <p>タイトル：{item.title}</p> */}
+          {/* <p>追加したもの：{item.email}</p> */}
+          <p>{item.name}</p>
+          <p>{item.kana}</p>
+          <p>{item.email}</p>
+          <p>{item.cue}</p>
+          <p>{item.reason}</p>
+          <p>{item.detail}</p>
+          <Button onClick={()=>handleDelete(index)}>削除</Button>
           {/* <Button type="remove" variant="contained" onClick={()=>handleDelete()}>削除</Button> */}
         </div>
       ))}
+       
+       <form onSubmit={allDelete}>
+        <Button type="submit" variant="contained">クリア</Button>
+      </form>
+
+
+
+
 
       {/* ここから書いていきます */}
     </div>
